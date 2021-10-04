90821f7e880632174d55e77d7ff499f94ec158f1
prasad <prasad2082001@gmail.com>
2021-10-04 13:41:09 +0530

    signup login code updatetd

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 function LoginPage(props) {
     return (
         <div>
-            Login page
+            <div className="login-wrapper">
+      <div className="login-form">
+        <h2> Login Form</h2>
+
+        <div className="username-container">
+          <label for="mail">Username:</label>
+
+          <input
+            type="email"
+            id="mail"
+            name="mail"
+            placeholder="Enter e-mail"
+          />
+        </div>
+
+        <div className="pass-container">
+          <label for="pass">Password:</label>
+          
+          <input
+            type="password"
+            id="pass"
+            name="password"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="remember-link">
+         <div>Remember me</div>  
+         <div>Resister Here</div>
+        
+</div>
+        
+
+        <div className="button-container">
+          <button className="button">Login</button>
+          
+        </div>
+        
+      </div>
+    </div>
         </div>
     );
 }
